e47b883da712265e89a9f6440dbc368f96170b74
Renan Santos <renan.santos@grupoeuro17.com.br>
2024-12-20 17:14:18 -0300

    criando classes

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,45 +1,56 @@
-import { Cliente } from "./Cliente.js";
+import {
+    Cliente
+} from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
 
-    #saldo = 0;
-    #cliente;
-    
-    constructor(cliente, ag) {
-        this.agencia = ag;
-        this.cliente = cliente;
-        ContaCorrente.numeroDeContas ++;
+    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+
+
+
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
     }
 
     get saldo() {
-        return this.#saldo;
+        return this._saldo;
     }
 
-    get cliente(){
-        return this.#cliente;
-    }
-    set cliente(novoValor){
-        if (novoValor instanceof Cliente) this.#cliente = novoValor;
-    }
-    
-
-    sacar(valor){
-        if (this.#saldo < valor) return "Saldo insuficiente";
-
-        this.#saldo -= valor;
-        return valor; 
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        this._saldo = 0;
+        ContaCorrente.numeroDeContas += 1;
     }
 
-    depositar(valor){
-        if (valor <= 0) return "Depósito inválido";
 
-        this.#saldo += valor;
-        return valor;
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
     }
 
-    transferir(valor, contaDestino){
+    depositar(valor) {
+        if (valor <= 100) {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+
         const valorSacado = this.sacar(valor);
-        contaDestino.depositar(valorSacado);
+        conta.depositar(valorSacado);
+
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Conta} from "./Conta.js";
 
-const cliente1 = new Cliente('renan', '39913800803');
-const contaCorrente1 = new ContaCorrente(cliente1, '6858-6');
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
+const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
+const contaPoupanca = new Conta(50, cliente1, 1001);
 
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
